bc7095fb1c2429e117d667eff69f94e47e9cdeaa
Alan Avellaneda <alansito1177@gmail.com>
2021-09-17 14:43:58 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" class="container">
+      <p>gamer</p>
+    </div>
+  );
+}
+
+export default App;
